2a749d1f4eb093a56c3d61d4fb62c5aa6b388b4e
Dm-Rb <135587971+Dm-Rb@users.noreply.github.com>
2025-06-10 16:00:36 +0300

    beta 1.0

--- callbacks.py
+++ callbacks.py
@@ -14,22 +14,20 @@ router = Router()
 async def handle_pagination(callback: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     current_page = data.get("current_page", 0)
-    items = data.get("items", [])  # Ваш массив cocktails_array
+    items = data.get("items", [])  # массив cocktails_array
 
     if callback.data == "prev_page":
-        current_page = max(0, current_page - 1)  # Не уходим ниже 0
+        current_page = max(0, current_page - 1)  # не уходим ниже 0
     elif callback.data == "next_page":
         current_page += 1
 
     await state.update_data(current_page=current_page)
 
-    # Генерируем новую клавиатуру
     keyboard = await build_keyboard_with_pagination(items, current_page)
 
-    # Редактируем сообщение (чтобы не создавать новое)
+    # редактируем сообщение (чтобы не создавать новое)
     await callback.message.edit_reply_markup(reply_markup=keyboard)
-    await callback.answer()  # Убираем "часики" в интерфейсе
-
+    await callback.answer() 
 
 @router.callback_query(F.data.startswith('item_'))
 async def handle_show_cocktail_details(callback: types.CallbackQuery):
